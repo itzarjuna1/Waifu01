1d515efa665c303244f7012c8585ec6dc44922f5
ARJUN <pachangedinanath@gmail.com>
2026-01-10 01:51:15 +0530

    Fix import statement in gupload.py

--- TEAMZYRO/modules/gupload.py
+++ TEAMZYRO/modules/gupload.py
@@ -1,7 +1,7 @@
 import asyncio
 import random
 from pyrogram import filters
-from TEAMZYRO import app, OWNER_ID, SUDO, db, 
+from TEAMZYRO import app, OWNER_ID, SUDO, db
 from TEAMZZYRO.__init__ import CHARA_CHANNEL_ID
 
 print("🔥 gupload module loaded")
